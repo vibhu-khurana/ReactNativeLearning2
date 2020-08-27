6216cce4e0b75645257740de1521d40c00aaf654
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-08-27 22:46:57 +0530

    resuable style component and layout changed

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, StyleSheet, TextInput, Text } from 'react-native';
+import { View, StyleSheet, TextInput, Text, Button } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>the game screen</Text>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title='Reset' onPress={() => {}} />
+          <Button title='Confirm' onPress={() => {}} />
+        </View>
+      </Card>
     </View>
   );
 };
@@ -14,6 +23,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: '300px',
+    maxWidth: '80%',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
   },
 });
 
